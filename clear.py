490ea201f57af56f6ef38721a5fcd7c5b7bb1ab9
Ev4squEz <41029552+Ev4squEz@users.noreply.github.com>
2023-10-12 12:55:26 -0300

    Se actualiza los modulos

--- clear.py
+++ clear.py
@@ -12,22 +12,15 @@ home: str = str(Path('~').expanduser())
 def clearDirTemp():
 
     pathTemp = "C:/Windows/Temp/"
-    pathTempWin = home+"/AppData/Local/Temp/"
-
-    processTemp = run(['du', '-sh', pathTemp], capture_output=True, text=True)
-    processTempWin = run(['du', '-sh', pathTempWin],
-                         capture_output=True, text=True)
-
-    sizeTemp = processTemp.stdout.split()
-    sizeTempWin = processTempWin.stdout.split()
+    pathTempWin = "C:/Users/usuario/AppData/Local/Temp/"
 
     elemTemp = os.listdir(pathTemp)
     elemTempWin = os.listdir(pathTempWin)
 
-    print("Elementos :", len(elemTemp), sizeTemp)
-    print("Elementos :", len(elemTempWin), sizeTempWin)
+    print("Elementos :", len(elemTemp))
+    print("Elementos :", len(elemTempWin))
 
-    # Recorre los elementos de la lista, que corresponde a los nombre de los archivos
+    # Recorre los elementos de la lista, que correspond a los nombre de los archivos
     # Se agrega la ruta mas el nombre del archivo
 
     with os.scandir(pathTemp) as ficheros:
@@ -89,210 +82,298 @@ def clearDirTemp():
 
 def clearDirUser():
 
-    pathDoc = home+"/Documents"
-    pathMusic = home+"/Music"
-    pathImg = home+"/Pictures"
-    pathVideo = home+"/Videos"
-    pathDownload = home+"/Downloads"
-    pathDesktop = home+"/Desktop"
+    homeUser = "C:/Users/usuario/"
 
-    processDoc = run(['du', '-sh', pathDoc], capture_output=True, text=True)
-    processMusic = run(['du', '-sh', pathMusic],
-                       capture_output=True, text=True)
-    processImg = run(['du', '-sh', pathImg], capture_output=True, text=True)
-    processVideo = run(['du', '-sh', pathVideo],
-                       capture_output=True, text=True)
-    processDownload = run(['du', '-sh', pathDownload],
-                          capture_output=True, text=True)
-    processDesktop = run(['du', '-sh', pathDesktop],
-                         capture_output=True, text=True)
-
-    sizeDoc = processDoc.stdout.split()
-    sizeMusic = processMusic.stdout.split()
-    sizeImg = processImg.stdout.split()
-    sizeVideo = processVideo.stdout.split()
-    sizeDownload = processDownload.stdout.split()
-    sizeDesktop = processDesktop.stdout.split()
-
-    elemDoc = os.listdir(pathDoc)
-    elemMusic = os.listdir(pathMusic)
-    elemImg = os.listdir(pathImg)
-    elemVideo = os.listdir(pathVideo)
-    elemDownload = os.listdir(pathDownload)
-    elemDesktop = os.listdir(pathDesktop)
-
-    print("Elementos en Documentos: ", len(elemDoc), " ->", sizeDoc)
-    print("Elementos en Musica: ", len(elemMusic), " ->", sizeMusic)
-    print("Elementos en Imagenes: ", len(elemImg), " ->", sizeImg)
-    print("Elementos en Video: ", len(elemVideo), " ->", sizeVideo)
-    print("Elementos en Download: ", len(elemDownload), " ->", sizeDownload)
-    print("Elementos en Desktop: ", len(elemDesktop), " ->", sizeDesktop)
-
-    # Recorre los elementos de la lista, que corresponde a los nombre de los archivos
-    # Se agrega la ruta mas el nombre del archivo
-
-    with os.scandir(pathDoc) as ficheros:
-        subdirectorios = [
-            fichero.name for fichero in ficheros if fichero.is_dir()]
-
-        for dir in subdirectorios:
-            dirDelete = os.path.join(pathDoc, dir)
-            print(dirDelete)
-
-            try:
-                shutil.rmtree(dirDelete)
-                print("Directory removed successfully")
-            except FileNotFoundError:
-                print("Directory does not exist")
-            except PermissionError:
-                print("Permission denied")
-            except Exception as e:
-                print(f"An error occurred: {str(e)}")
-
-    with os.scandir(pathMusic) as ficheros:
-        subdirectorios = [
-            fichero.name for fichero in ficheros if fichero.is_dir()]
-
-        for dir in subdirectorios:
-            dirDelete = os.path.join(pathMusic, dir)
-            print(dirDelete)
-
-            try:
-                shutil.rmtree(dirDelete)
-                print("Directory removed successfully")
-            except FileNotFoundError:
-                print("Directory does not exist")
-            except PermissionError:
-                print("Permission denied")
-            except Exception as e:
-                print(f"An error occurred: {str(e)}")
-
-    with os.scandir(pathImg) as ficheros:
-        subdirectorios = [
-            fichero.name for fichero in ficheros if fichero.is_dir()]
-
-        for dir in subdirectorios:
-            dirDelete = os.path.join(pathImg, dir)
-            print(dirDelete)
-
-            try:
-                shutil.rmtree(dirDelete)
-                print("Directory removed successfully")
-            except FileNotFoundError:
-                print("Directory does not exist")
-            except PermissionError:
-                print("Permission denied")
-            except Exception as e:
-                print(f"An error occurred: {str(e)}")
-
-    with os.scandir(pathVideo) as ficheros:
-        subdirectorios = [
-            fichero.name for fichero in ficheros if fichero.is_dir()]
-
-        for dir in subdirectorios:
-            dirDelete = os.path.join(pathVideo, dir)
-            print(dirDelete)
-
-            try:
-                shutil.rmtree(dirDelete)
-                print("Directory removed successfully")
-            except FileNotFoundError:
-                print("Directory does not exist")
-            except PermissionError:
-                print("Permission denied")
-            except Exception as e:
-                print(f"An error occurred: {str(e)}")
-
-    with os.scandir(pathDownload) as ficheros:
-        subdirectorios = [
-            fichero.name for fichero in ficheros if fichero.is_dir()]
-
-        for dir in subdirectorios:
-            dirDelete = os.path.join(pathDownload, dir)
-            print(dirDelete)
-
-            try:
-                shutil.rmtree(dirDelete)
-                print("Directory removed successfully")
-            except FileNotFoundError:
-                print("Directory does not exist")
-            except PermissionError:
-                print("Permission denied")
-            except Exception as e:
-                print(f"An error occurred: {str(e)}")
-
-    with os.scandir(pathDesktop) as ficheros:
-        subdirectorios = [
-            fichero.name for fichero in ficheros if fichero.is_dir()]
-
-        for dir in subdirectorios:
-            dirDelete = os.path.join(pathDesktop, dir)
-            print(dirDelete)
-
-            try:
-                shutil.rmtree(dirDelete)
-                print("Directory removed successfully")
-            except FileNotFoundError:
-                print("Directory does not exist")
-            except PermissionError:
-                print("Permission denied")
-            except Exception as e:
-                print(f"An error occurred: {str(e)}")
-
+    countOne = 0
     countDoc = 0
+    countDocume = 0
     countMusic = 0
     countImg = 0
     countVideo = 0
     countDownload = 0
     countDesktop = 0
+    countDesktop_1 = 0
 
-    for doc in elemDoc:
-        eliminarDoc = os.path.join(pathDoc, doc)
-        try:
-            os.remove(eliminarDoc)
-        except:
-            countDoc += 1
+    # Recorre los elementos de la lista, que corresponde a los nombre de los archivos
+    # Se agrega la ruta mas el nombre del archivo
 
-    for music in elemMusic:
-        eliminarMusic = os.path.join(pathMusic, music)
-        try:
-            os.remove(eliminarMusic)
-        except:
-            countMusic += 1
+    pathOne = homeUser+"OneDrive"
+    if not os.path.exists(pathOne):
+        print("No encontrado")
+    else:
+        elemOne = os.listdir(pathOne)
+        with os.scandir(pathOne) as ficheros:
+            subdirectorios = [
+                fichero.name for fichero in ficheros if fichero.is_dir()]
 
-    for img in elemImg:
-        eliminarImg = os.path.join(pathImg, img)
-        try:
-            os.remove(eliminarImg)
-        except:
-            countImg += 1
+            for dir in subdirectorios:
+                dirDelete = os.path.join(pathOne, dir)
+                print(dirDelete)
 
-    for video in elemVideo:
-        eliminarVideo = os.path.join(pathVideo, video)
-        try:
-            os.remove(eliminarVideo)
-        except:
-            countVideo += 1
+                try:
+                    shutil.rmtree(dirDelete)
+                    print("Directory removed successfully")
+                except FileNotFoundError:
+                    print("Directory does not exist")
+                except PermissionError:
+                    print("Permission denied")
+                except Exception as e:
+                    print(f"An error occurred: {str(e)}")
 
-    for down in elemDownload:
-        eliminarDown = os.path.join(pathDownload, down)
-        try:
-            os.remove(eliminarDown)
-        except:
-            countDownload += 1
-    for desk in elemDesktop:
-        eliminarDesk = os.path.join(pathDesktop, desk)
-        try:
-            os.remove(eliminarDesk)
-        except:
-            countDesktop += 1
+        for one in elemOne:
+            eliminarOne = os.path.join(pathOne, one)
+            try:
+                os.remove(eliminarOne)
+            except:
+                countOne += 1
 
-    print("Error", countDoc, sizeDoc)
-    print("Error", countMusic, sizeMusic)
-    print("Error", countImg, sizeImg)
-    print("Error", countVideo, sizeVideo)
-    print("Error", countDownload, sizeDownload)
-    print("Error", countDesktop, sizeDesktop)
+    pathDownload = homeUser+"Downloads"
+    if not os.path.exists(pathDownload):
+        print("No encontrado")
+    else:
+        elemDownload = os.listdir(pathDownload)
+        with os.scandir(pathDownload) as ficheros:
+            subdirectorios = [
+                fichero.name for fichero in ficheros if fichero.is_dir()]
+
+            for dir in subdirectorios:
+                dirDelete = os.path.join(pathDownload, dir)
+                print(dirDelete)
+
+                try:
+                    shutil.rmtree(dirDelete)
+                    print("Directory removed successfully")
+                except FileNotFoundError:
+                    print("Directory does not exist")
+                except PermissionError:
+                    print("Permission denied")
+                except Exception as e:
+                    print(f"An error occurred: {str(e)}")
+
+        for down in elemDownload:
+            eliminarDown = os.path.join(pathDownload, down)
+            try:
+                os.remove(eliminarDown)
+            except:
+                countDownload += 1
+
+    pathDoc = homeUser+"Documentos"
+    if not os.path.exists(pathDoc):
+        print("No encontrado")
+    else:
+        elemDoc = os.listdir(pathDoc)
+        with os.scandir(pathDoc) as ficheros:
+            subdirectorios = [
+                fichero.name for fichero in ficheros if fichero.is_dir()]
+
+            for dir in subdirectorios:
+                dirDelete = os.path.join(pathDoc, dir)
+                print(dirDelete)
+
+                try:
+                    shutil.rmtree(dirDelete)
+                    print("Directory removed successfully")
+                except FileNotFoundError:
+                    print("Directory does not exist")
+                except PermissionError:
+                    print("Permission denied")
+                except Exception as e:
+                    print(f"An error occurred: {str(e)}")
+
+        for doc in elemDoc:
+            eliminarDoc = os.path.join(pathDoc, doc)
+            try:
+                os.remove(eliminarDoc)
+            except:
+                countDoc += 1
+
+    pathDocume = homeUser+"Documents"
+    if not os.path.exists(pathDocume):
+        print("No encontrado")
+    else:
+        elemDocume = os.listdir(pathDocume)
+        with os.scandir(pathDocume) as ficheros:
+            subdirectorios = [
+                fichero.name for fichero in ficheros if fichero.is_dir()]
+
+            for dir in subdirectorios:
+                dirDelete = os.path.join(pathDocume, dir)
+                print(dirDelete)
+
+                try:
+                    shutil.rmtree(dirDelete)
+                    print("Directory removed successfully")
+                except FileNotFoundError:
+                    print("Directory does not exist")
+                except PermissionError:
+                    print("Permission denied")
+                except Exception as e:
+                    print(f"An error occurred: {str(e)}")
+
+        for doc in elemDocume:
+            eliminarDoc = os.path.join(pathDocume, doc)
+            try:
+                os.remove(eliminarDoc)
+            except:
+                countDocume += 1
+
+    pathDesktop = homeUser+"Escritorio"
+    if not os.path.exists(pathDesktop):
+        print("No encontrado")
+    else:
+        elemDesktop = os.listdir(pathDesktop)
+        with os.scandir(pathDesktop) as ficheros:
+            subdirectorios = [
+                fichero.name for fichero in ficheros if fichero.is_dir()]
+
+            for dir in subdirectorios:
+                dirDelete = os.path.join(pathDesktop, dir)
+                print(dirDelete)
+
+                try:
+                    shutil.rmtree(dirDelete)
+                    print("Directory removed successfully")
+                except FileNotFoundError:
+                    print("Directory does not exist")
+                except PermissionError:
+                    print("Permission denied")
+                except Exception as e:
+                    print(f"An error occurred: {str(e)}")
+
+        for desk in elemDesktop:
+            eliminarDesk = os.path.join(pathDesktop, desk)
+            try:
+                os.remove(eliminarDesk)
+            except:
+                countDesktop += 1
+
+    pathDesktop_1 = homeUser+"Desktop"
+    if not os.path.exists(pathDesktop_1):
+        print("No encontrado")
+    else:
+        elemDesktop = os.listdir(pathDesktop_1)
+        with os.scandir(pathDesktop_1) as ficheros:
+            subdirectorios = [
+                fichero.name for fichero in ficheros if fichero.is_dir()]
+
+            for dir in subdirectorios:
+                dirDelete = os.path.join(pathDesktop_1, dir)
+                print(dirDelete)
+
+                try:
+                    shutil.rmtree(dirDelete)
+                    print("Directory removed successfully")
+                except FileNotFoundError:
+                    print("Directory does not exist")
+                except PermissionError:
+                    print("Permission denied")
+                except Exception as e:
+                    print(f"An error occurred: {str(e)}")
+
+        for desk in elemDesktop:
+            eliminarDesk = os.path.join(pathDesktop_1, desk)
+            try:
+                os.remove(eliminarDesk)
+            except:
+                countDesktop_1 += 1
+
+    pathMusic = homeUser+"Music"
+    if not os.path.exists(pathMusic):
+        print("No encontrado")
+    else:
+        elemMusic = os.listdir(pathMusic)
+        with os.scandir(pathMusic) as ficheros:
+            subdirectorios = [
+                fichero.name for fichero in ficheros if fichero.is_dir()]
+
+            for dir in subdirectorios:
+                dirDelete = os.path.join(pathMusic, dir)
+                print(dirDelete)
+
+                try:
+                    shutil.rmtree(dirDelete)
+                    print("Directory removed successfully")
+                except FileNotFoundError:
+                    print("Directory does not exist")
+                except PermissionError:
+                    print("Permission denied")
+                except Exception as e:
+                    print(f"An error occurred: {str(e)}")
+
+        for music in elemMusic:
+            eliminarMusic = os.path.join(pathMusic, music)
+            try:
+                os.remove(eliminarMusic)
+            except:
+                countMusic += 1
+
+    pathImg = homeUser+"Pictures"
+    if not os.path.exists(pathImg):
+        print("No encontrado")
+    else:
+        elemImg = os.listdir(pathImg)
+        with os.scandir(pathImg) as ficheros:
+            subdirectorios = [
+                fichero.name for fichero in ficheros if fichero.is_dir()]
+
+            for dir in subdirectorios:
+                dirDelete = os.path.join(pathImg, dir)
+                print(dirDelete)
+
+                try:
+                    shutil.rmtree(dirDelete)
+                    print("Directory removed successfully")
+                except FileNotFoundError:
+                    print("Directory does not exist")
+                except PermissionError:
+                    print("Permission denied")
+                except Exception as e:
+                    print(f"An error occurred: {str(e)}")
+
+        for img in elemImg:
+            eliminarImg = os.path.join(pathImg, img)
+            try:
+                os.remove(eliminarImg)
+            except:
+                countImg += 1
+
+    pathVideo = homeUser+"Videos"
+    if not os.path.exists(pathVideo):
+        print("No encontrado")
+    else:
+        elemVideo = os.listdir(pathVideo)
+        with os.scandir(pathVideo) as ficheros:
+            subdirectorios = [
+                fichero.name for fichero in ficheros if fichero.is_dir()]
+
+            for dir in subdirectorios:
+                dirDelete = os.path.join(pathVideo, dir)
+                print(dirDelete)
+
+                try:
+                    shutil.rmtree(dirDelete)
+                    print("Directory removed successfully")
+                except FileNotFoundError:
+                    print("Directory does not exist")
+                except PermissionError:
+                    print("Permission denied")
+                except Exception as e:
+                    print(f"An error occurred: {str(e)}")
+
+        for video in elemVideo:
+            eliminarVideo = os.path.join(pathVideo, video)
+            try:
+                os.remove(eliminarVideo)
+            except:
+                countVideo += 1
+
+    print("Error", countDoc)
+    print("Error", countMusic)
+    print("Error", countImg)
+    print("Error", countVideo)
+    print("Error", countDownload)
+    print("Error", countDesktop)
+    print("Error", countOne)
 
 
 def clearGoogleChrome():
@@ -302,8 +383,7 @@ def clearGoogleChrome():
 
     browserExe = "chrome.exe"
     os.system("taskkill /f /im "+browserExe)
-
-    pathBrowser = home+"/AppData/Local/Google/Chrome/User Data/"
+    pathBrowser = "C:/Users/usuario/AppData/Local/Google/Chrome/User Data/"
 
     with os.scandir(pathBrowser) as ficheros:
         subdirectorios = [
